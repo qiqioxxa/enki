78109fe5b4bae9c1d64fb54dffd2e32fc7a1af46
qiqioxxa <mtuboltcev4@gmail.com>
2026-04-05 01:59:43 +0300

    perf: eliminate dynamic memory in move generation
    - replace std::vector with stack-allocated MoveList to eliminate heap allocations
    - rename UndoMove to UnmakeMove for consistency
    - remove redundant helpers: is_pawn(), is_king(), is_rook(), is_current_player_piece()
    - extract attack table initialization to AttackTables class
    - benchmark: 1.72M nodes/sec

---
+++ attack_tables.cpp
@@ -0,0 +1,152 @@
+#include "attack_tables.h"
+
+// PUBLIC
+
+void AttackTables::init() {
+    for (int square = 0; square < 64; square++) {
+        pawn_pushes[0][square] = compute_pawn_pushes_from(square, true);
+        pawn_pushes[1][square] = compute_pawn_pushes_from(square, false);
+        pawn_attacks[0][square] = compute_pawn_attacks_from(square, true);
+        pawn_attacks[1][square] = compute_pawn_attacks_from(square, false);
+        knight_attacks[square] = compute_knight_attacks_from(square);
+        king_attacks[square] = compute_king_attacks_from(square);
+    }
+
+    initialize_masks();
+
+    const std::array<std::pair<int, int>, 4> bishop_dirs = {{ {-1, -1}, {-1, 1}, {1, -1}, {1, 1} }};
+    const std::array<std::pair<int, int>, 4> rook_dirs = {{ {-1, 0}, {1, 0}, {0, -1}, {0, 1} }};
+    
+    for (int square = 0; square < 64; ++square) {
+        uint64_t mask = bishop_masks[square];
+        uint64_t cur_blockers = 0;
+        
+        do {
+            uint64_t index = (cur_blockers * bishop_magics[square]) >> (64 - bishop_shifts[square]);
+            bishop_attacks[square][index] = compute_sliding_attacks_from(square, bishop_dirs, cur_blockers);
+            cur_blockers = (cur_blockers - mask) & mask;
+        } while (cur_blockers != 0);
+        
+        mask = rook_masks[square];
+        cur_blockers = 0;
+        
+        do {
+            uint64_t index = (cur_blockers * rook_magics[square]) >> (64 - rook_shifts[square]);
+            rook_attacks[square][index] = compute_sliding_attacks_from(square, rook_dirs, cur_blockers);
+            cur_blockers = (cur_blockers - mask) & mask;
+        } while (cur_blockers != 0);
+    }
+}
+
+// PRIVATE
+
+void AttackTables::initialize_masks() {
+    for (int square = 0; square < 64; square++) {
+        uint64_t bishop_mask = 0, rook_mask = 0;
+        int rank = square / 8, file = square % 8;
+        
+        for (int r = rank + 1, f = file + 1; r < 7 && f < 7; r++, f++) bishop_mask |= (1ULL << (r * 8 + f));
+        for (int r = rank - 1, f = file - 1; r > 0 && f > 0; r--, f--) bishop_mask |= (1ULL << (r * 8 + f));
+        for (int r = rank + 1, f = file - 1; r < 7 && f > 0; r++, f--) bishop_mask |= (1ULL << (r * 8 + f));
+        for (int r = rank - 1, f = file + 1; r > 0 && f < 7; r--, f++) bishop_mask |= (1ULL << (r * 8 + f));
+        bishop_masks[square] = bishop_mask;
+        
+        for (int r = rank + 1; r < 7; r++) rook_mask |= (1ULL << (r * 8 + file));
+        for (int r = rank - 1; r > 0; r--) rook_mask |= (1ULL << (r * 8 + file));
+        for (int f = file + 1; f < 7; f++) rook_mask |= (1ULL << (rank * 8 + f));
+        for (int f = file - 1; f > 0; f--) rook_mask |= (1ULL << (rank * 8 + f));
+        rook_masks[square] = rook_mask;
+    }
+}
+
+uint64_t AttackTables::compute_pawn_pushes_from(int square, bool white) {
+    uint64_t pushes = 0;
+    int rank = square / 8;
+    int file = square % 8;
+    for (int target = 0; target < 64; target++) {
+        if (target == square) continue;
+        int rank_diff = target / 8 - rank;
+        int file_diff = target % 8 - file;
+        if (white) {
+            if (rank_diff == 1 && file_diff == 0) {
+                pushes |= (1ULL << target);
+            }
+            if (rank == 1 && rank_diff == 2 && file_diff == 0) {
+                pushes |= (1ULL << target);
+            }
+        } else {
+            if (rank_diff == -1 && file_diff == 0) {
+                pushes |= (1ULL << target);
+            }
+            if (rank == 6 && rank_diff == -2 && file_diff == 0) {
+                pushes |= (1ULL << target);
+            }
+        }
+    }
+    return pushes;
+}
+uint64_t AttackTables::compute_pawn_attacks_from(int square, bool white) {
+    uint64_t attacks = 0;
+    int rank = square / 8;
+    int file = square % 8;
+    for (int target = 0; target < 64; target++) {
+        if (target == square) continue;
+        int rank_diff = target / 8 - rank;
+        int file_diff = target % 8 - file;
+        if (white) {
+            if ((rank_diff == 1 && file_diff == 1) || (rank_diff == 1 && file_diff == -1)) {
+                attacks |= (1ULL << target);
+            }
+        } else {
+            if ((rank_diff == -1 && file_diff == 1) || (rank_diff == -1 && file_diff == -1)) {
+                attacks |= (1ULL << target);
+            }
+        }
+    }
+    return attacks;
+}
+uint64_t AttackTables::compute_knight_attacks_from(int square) {
+    uint64_t attacks = 0;
+    int rank = square / 8;
+    int file = square % 8;
+    for (int target = 0; target < 64; target++) {
+        if (target == square) continue;
+        int rank_diff = abs(target / 8 - rank);
+        int file_diff = abs(target % 8 - file);
+        if ((rank_diff == 1 && file_diff == 2) || (rank_diff == 2 && file_diff == 1)) {
+            attacks |= (1ULL << target);
+        }
+    }
+    return attacks;
+}
+uint64_t AttackTables::compute_king_attacks_from(int square) {
+    uint64_t attacks = 0;
+    int rank = square / 8;
+    int file = square % 8;
+    for (int target = 0; target < 64; target++) {
+        if (target == square) continue;
+        int rank_diff = abs(target / 8 - rank);
+        int file_diff = abs(target % 8 - file);
+        if (rank_diff <= 1 && file_diff <= 1) {
+            attacks |= (1ULL << target);
+        }
+    }
+    return attacks;
+}
+uint64_t AttackTables::compute_sliding_attacks_from(int square, const std::array<std::pair<int, int>, 4>& directions, uint64_t blockers) {
+    uint64_t attacks = 0;
+    int rank = square / 8;
+    int file = square % 8;
+    for (const auto& dir : directions) {
+        int r = rank;
+        int f = file;
+        while (true) {
+            r += dir.first;
+            f += dir.second;
+            if (r < 0 || r > 7 || f < 0 || f > 7) break;
+            attacks |= (1ULL << (r * 8 + f));
+            if (blockers & (1ULL << (r * 8 + f))) break;
+        }
+    }
+    return attacks;
+}
--- board.h
+++ board.h
@@ -3,7 +3,6 @@
 #include <cstdint>
 #include <array>
 #include <map>
-#include <vector>
 #include <string>
 #include <iostream>
 
@@ -15,6 +14,8 @@ enum class Piece {
 
 struct Move {
     uint16_t data; // ceppttttttffffff
+
+    Move() : data(0) {}
 
     Move(int from, int to, int promotion = 0, bool ep = false, bool castle = false) {
         data = (from & 0x3F) | 
@@ -54,7 +55,7 @@ struct Move {
     }
 };
 
-struct UndoInfo {
+struct UnmakeInfo {
     uint8_t en_passant_square;
     uint8_t white_king_square;
     uint8_t black_king_square;
@@ -64,18 +65,36 @@ struct UndoInfo {
     int8_t target_bb;
 };
 
+struct MoveList {
+    Move moves[256];
+    int size = 0;
+
+    inline void add(Move move) { moves[size++] = move; }
+    inline void clear() { size = 0; }
+    inline Move& operator[](int i) { return moves[i]; }
+    inline const Move& operator[](int i) const { return moves[i]; }
+
+    inline Move* begin() { return moves; }
+    inline Move* end() { return moves + size; }
+    inline const Move* begin() const { return moves; }
+    inline const Move* end() const { return moves + size; }
+};
+
 class ChessBoard {
 public:
     ChessBoard(bool standard = true);
 
-    UndoInfo make_move(const Move move);
+    UnmakeInfo make_move(const Move move);
     bool make_move(const std::string& str_move);
-    void unmake_move(const Move move, const UndoInfo& info);
+    void unmake_move(const Move move, const UnmakeInfo& info);
 
-    std::vector<Move> generate_valid_moves() const;
+    void generate_valid_moves(MoveList& list) const;
 
-    void set_turn(bool white);
-    void set_piece(const std::string& notation, Piece piece);
+    bool is_check() const;
+    bool is_checkmate() const;
+    bool is_stalemate() const;
+    bool is_fifty_moves_rule() const;
+    bool is_game_over() const;
 
     uint64_t get_pawns(bool white) const;
     uint64_t get_knights(bool white) const;
@@ -90,67 +109,27 @@ public:
     uint64_t get_empty_squares() const;
     const std::array<uint64_t, 12>& get_bitboards() const;
 
-    bool is_check() const;
-    bool is_checkmate() const;
-    bool is_stalemate() const;
-    bool is_fifty_moves_rule() const;
-    bool is_game_over() const;
-
-    static bool is_pawn(Piece piece);
-    static bool is_king(Piece piece);
-    static bool is_rook(Piece piece);
+    void set_piece(const std::string& notation, Piece piece);
+    void set_turn(bool white);
 
     void print_board(bool flipping = false) const;
     void print_moves() const;
+
     void perft_divide(int depth);
     std::array<uint64_t, 7> perft(int depth);
 
     static int notation_to_square(const std::string& notation);
     static std::string square_to_notation(int square);
+
     static int pop_lsb(uint64_t& bb);
-    
+
 private:
-    mutable std::array<uint64_t, 12> bitboards{};
-    bool white_turn = true;
-    int en_passant_square = -1;
-    mutable int white_king_square = -1;
-    mutable int black_king_square = -1;
-    bool white_queenside_castle = false;
-    bool white_kingside_castle = false;
-    bool black_queenside_castle = false;
-    bool black_kingside_castle = false;
-    int reversible_moves_count = 0;
-    int total_moves_count = 0;
-
-    static const std::array<uint64_t, 64> bishop_magics;
-    static const std::array<uint64_t, 64> rook_magics;
-    static const std::array<int, 64> bishop_shifts;
-    static const std::array<int, 64> rook_shifts;
-
-    static std::array<uint64_t, 64> bishop_masks;
-    static std::array<uint64_t, 64> rook_masks;
-    static std::array<std::array<uint64_t, 64>, 2> pawn_pushes;
-    static std::array<std::array<uint64_t, 64>, 2> pawn_attacks;
-    static std::array<uint64_t, 64> knight_attacks;
-    static std::array<uint64_t, 64> king_attacks;
-    static std::array<std::array<uint64_t, 512>, 64> bishop_attacks;
-    static std::array<std::array<uint64_t, 4096>, 64> rook_attacks;
-    static inline bool tables_initialized = false;
-
-
     void standard_setup();
-    static void initialize_masks();
-    static void initialize_attack_tables();
-    static uint64_t compute_pawn_pushes_from(int square, bool white);
-    static uint64_t compute_pawn_attacks_from(int square, bool white);
-    static uint64_t compute_knight_attacks_from(int square);
-    static uint64_t compute_king_attacks_from(int square);
-    static uint64_t compute_sliding_attacks_from(int square, const std::array<std::pair<int, int>, 4>& directions, uint64_t blockers);
 
-    inline void execute_pseudo_move(const Move move, int moving_bb, int target_bb) const;
+    void execute_pseudo_move(const Move move, int moving_bb, int target_bb);
     void update_flags_and_counters(const Move move, int moving_bb, int target_bb);
 
-    std::vector<Move> generate_pseudo_moves() const;
+    void generate_pseudo_moves(MoveList& list) const;
     uint64_t generate_pawn_moves_from(int square, bool white) const;
     uint64_t generate_knight_moves_from(int square, bool white) const;
     uint64_t generate_bishop_moves_from(int square, bool white) const;
@@ -167,7 +146,21 @@ private:
     int piece_to_bb_index(Piece piece) const;
     Piece bb_index_to_piece(int bb_index) const;
 
-    bool is_current_player_piece(Piece piece) const;
-    bool is_square_attacked(int square, bool by_white) const;
     bool can_castle(int king_square, int target) const;
+    bool is_square_attacked(int square, bool by_white) const;
+
+
+    static inline bool tables_initialized = false;
+
+    std::array<uint64_t, 12> bitboards{};
+    bool white_turn = true;
+    int en_passant_square = -1;
+    int white_king_square = -1;
+    int black_king_square = -1;
+    bool white_queenside_castle = false;
+    bool white_kingside_castle = false;
+    bool black_queenside_castle = false;
+    bool black_kingside_castle = false;
+    int reversible_moves_count = 0;
+    int total_moves_count = 0;
 };
